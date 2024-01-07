d54f156e4029b44c71699fb2ae1141e704d03566
aescacena <aescacena@gmail.com>
2024-01-07 17:45:41 +0100

    DESARROLLO. Se para evolucionar el mundo, se crea un mundo con todas las células muertas y se van agregando las células que deben estar vivas creando el nuevo estado

--- src/Game.ts
+++ src/Game.ts
@@ -5,6 +5,43 @@ export class Game{
         this.world = world
     }
 
+    static createAllDead(rows: number, columns: number) {
+        let world: boolean[][] = Array(rows).fill([]).map(() => Array(columns).fill(false));
+        return new Game(world);
+    }
+
+    live(row: number, colum: number): void{
+        this.world[row][colum] = true;
+    }
+
+    dead(row: number, colum: number): void{
+        this.world[row][colum] = false;
+    }
+
+    actualState(): boolean[][]{
+        return this.world;
+    }
+
+    evolves(): void{
+        const newState: Game = Game.createAllDead(this.world.length, this.world[0].length);
+        for (let row = 0; row < this.world.length; row++) {
+            for (let column = 0; column < this.world[0].length; column++){
+                if(this.isDead(row, column)){
+                    if(this.shouldRevive(row, column)){
+                        newState.live(row, column);
+                    }
+                }else {
+                    if(this.shouldDead(row, column)){
+                        newState.dead(row, column);
+                    }else {
+                        newState.live(row, column);
+                    }
+                }
+            }
+        }
+        this.world = newState.world;
+    }
+
     getTotalLiveCells(): number{
         let totalLiveNeighbors = 0;
         for (let i = 0; i < this.world.length; i++) {
@@ -33,26 +70,29 @@ export class Game{
         return this.isPossibleRow(row) && this.isPossibleColum(colum) && this.world[row][colum];
     }
 
-    shouldLive(colum: number, row: number): boolean{
-        const totalLiveNeighbors = this.totalLivingNeighbors(colum, row);
-        return (totalLiveNeighbors == 2 || totalLiveNeighbors == 3);
+    private isDead(row: number, colum: number): boolean{
+        return this.isPossibleRow(row) && this.isPossibleColum(colum) && !this.world[row][colum];
     }
 
-    shouldDead(colum: number, row: number): boolean{
-        const totalLiveNeighbors = this.totalLivingNeighbors(colum, row);
+    shouldRevive(row: number, colum: number): boolean{
+        return this.totalLivingNeighbors(row, colum) == 3;
+    }
+
+    shouldDead(row: number, colum: number): boolean{
+        const totalLiveNeighbors = this.totalLivingNeighbors(row, colum);
         return (totalLiveNeighbors < 2 || totalLiveNeighbors > 3);
     }
 
-    private totalLivingNeighbors(colum: number, row: number){
-        const possibleColumns  = 3;
-        const possibleRows     = 3;
-        let totalLiveNeighbors = 0;
-        for (let actualColumn = colum - 1; actualColumn < ((colum - 1) + possibleColumns); actualColumn++) {
-            for (let actualRow = row - 1; actualRow < ((row - 1) + possibleRows); actualRow++) {
-                if(this.isAlive(actualRow, actualColumn) && ((row != actualRow) || (colum != actualColumn))){
-                    totalLiveNeighbors++;
-                }
-            }
+    private totalLivingNeighbors(row: number, colum: number){
+        const possibleColumns    = 3;
+        const possibleRows       = 3;
+        let   totalLiveNeighbors = 0;
+        for (let actualRow = row - 1; actualRow < ((row - 1) + possibleRows); actualRow++){
+             for (let actualColumn = colum - 1; actualColumn < ((colum - 1) + possibleColumns); actualColumn++) {
+                 if(this.isAlive(actualRow, actualColumn) && ((row != actualRow) || (colum != actualColumn))){
+                     totalLiveNeighbors++;
+                 }
+             }
         }
         return totalLiveNeighbors;
     }
--- tests/Game.test.ts
+++ tests/Game.test.ts
@@ -1,61 +1,169 @@
 import {GameMother} from "./GameMother";
+import {faker} from "@faker-js/faker";
 
 describe("Game of life", () => {
-    it("Should live that cell", () =>{
-        // Arrange
-        const world = [
-            [false, true , false, false],
-            [false, true , false, false],
-            [true , false, false, false],
-            [false, false, false, false]
-        ];
-        const game = GameMother.create(world);
+    describe("State cells", () => {
+        it("Should revive that cell because have 3 neighbors", () =>{
+            // Arrange
+            const world = [
+                [false, true , false, false],
+                [false, true , false, false],
+                [true , false, false, false],
+                [false, false, false, false]
+            ];
+            const game = GameMother.create(world);
 
-        // Act
-        const cellState = game.shouldLive(1, 1);
+            // Act
+            const cellState = game.shouldRevive(1, 0);
 
-        // Assert
-        expect(cellState).toBeTruthy();
+            // Assert
+            expect(cellState).toBeTruthy();
+        });
+
+        it("Should dead that cell", () =>{
+            // Arrange
+            const world = [
+                [false, true , false, false],
+                [false, true , false, false ],
+                [true , false, false, false],
+                [false, false, false, false]
+            ];
+            const game = GameMother.create(world);
+
+            // Act
+            const cellState = game.shouldDead(2, 0);
+
+            // Assert
+            expect(cellState).toBeTruthy();
+        });
+
+        it("Should obtain 20 live cells", () =>{
+            // Arrange
+            const liveCellsExpect = 20;
+            const game            = GameMother.withNumberLiveCells(liveCellsExpect);
+
+            // Act
+            const numberLiveCells = game.getTotalLiveCells();
+
+            // Assert
+            expect(liveCellsExpect).toEqual(numberLiveCells);
+        });
+
+        it("Should obtain 20 dead cells", () =>{
+            // Arrange
+            const deadCellsExpect = 20;
+            const game            = GameMother.withNumberLiveCells(deadCellsExpect);
+
+            // Act
+            const numberLiveCells = game.getTotalDeadCells();
+
+            // Assert
+            expect(deadCellsExpect).toEqual(numberLiveCells);
+        });
     });
 
-    it("Should dead that cell", () =>{
-        // Arrange
-        const world = [
-            [false, true , false, false],
-            [false, true, false, false ],
-            [true , false, false, false],
-            [false, false, false, false]
-        ];
-        const game = GameMother.create(world);
+    describe("State game", () => {
+        it("should obtain state with all cells dead, because living cells have no neighbors", () =>{
+            // Arrange
+            const world = [
+                [false, false, false, false],
+                [false, true , false, false],
+                [false, false, false, true],
+                [false, false, false, false]
+            ];
+            const worldExpect = [
+                [false, false, false, false],
+                [false, false, false, false],
+                [false, false, false, false],
+                [false, false, false, false]
+            ];
+            const game = GameMother.create(world);
 
-        // Act
-        const cellState = game.shouldDead(0, 2);
+            // Act
+            game.evolves();
 
-        // Assert
-        expect(cellState).toBeTruthy();
-    });
+            // Assert
+            expect(worldExpect).toEqual(game.actualState());
+        });
 
-    it("Should obtain 20 live cells", () =>{
-        // Arrange
-        const liveCellsExpect = 20;
-        const game            = GameMother.withNumberLiveCells(liveCellsExpect);
+        it("should obtain state with all cells dead, because living cells only have 1 neighbor", () =>{
+            // Arrange
+            const world = [
+                [false, false, false, false],
+                [false, true , true , false],
+                [false, false, false, false],
+                [false, false, false, false]
+            ];
+            const worldExpect = [
+                [false, false, false, false],
+                [false, false, false, false],
+                [false, false, false, false],
+                [false, false, false, false]
+            ];
+            const game = GameMother.create(world);
 
-        // Act
-        const numberLiveCells = game.getTotalLiveCells();
+            // Act
+            game.evolves();
 
-        // Assert
-        expect(liveCellsExpect).toEqual(numberLiveCells);
-    });
+            // Assert
+            expect(worldExpect).toEqual(game.actualState());
+        });
 
-    it("Should obtain 20 dead cells", () =>{
-        // Arrange
-        const deadCellsExpect = 20;
-        const game            = GameMother.withNumberLiveCells(deadCellsExpect);
+        it("should obtain same state, because all cells have 3 neighbors", () =>{
+            // Arrange
+            const world = [
+                [false, false, false, false],
+                [false, true , true , false],
+                [false, true , true , false],
+                [false, false, false, false]
+            ];
+            const worldExpect = [
+                [false, false, false, false],
+                [false, true , true , false],
+                [false, true , true , false],
+                [false, false, false, false]
+            ];
+            const game = GameMother.create(world);
+            const numberEvolves = faker.number.int({min: 2, max: 10});
 
-        // Act
-        const numberLiveCells = game.getTotalDeadCells();
+            // Act
+            for (let i = 0; i < numberEvolves; i++) {
+                game.evolves();
+            }
 
-        // Assert
-        expect(deadCellsExpect).toEqual(numberLiveCells);
+            // Assert
+            expect(worldExpect).toEqual(game.actualState());
+        });
+
+        it("should obtain sames states", () =>{
+            // Arrange
+            const firstState = [
+                [false, false, false, false],
+                [false, true , false, false],
+                [false, true , false, false],
+                [false, true , false, false],
+                [false, false, false, false]
+            ];
+            const secondState = [
+                [false, false, false, false],
+                [false, false, false, false],
+                [true , true , true , false],
+                [false, false, false, false],
+                [false, false, false, false]
+            ];
+            const game = GameMother.create(firstState);
+
+            // Act
+
+            // Assert
+            for (let i = 0; i < 4; i++) {
+                game.evolves();
+                if((i % 2) == 0){
+                    expect(secondState).toEqual(game.actualState());
+                }else {
+                    expect(firstState).toEqual(game.actualState());
+                }
+            }
+        });
     });
 })
